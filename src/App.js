08fdd43fb098e8d83b90424ede5e8d9371d37563
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-06 03:31:07 +0100

    tailwind css is okay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-xl font-bold underline'>App</div>
-  )
-}
+    <div className="text-xl font-bold underline bg-red-400">
+      Did this to test your tailwind installation. Everything is fine. You can
+      now proceed with the project please. Cheers,
+    </div>
+  );
+};
 
-export default App
+export default App;

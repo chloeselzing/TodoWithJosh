32ac75b456a9521110cdabefbc7c87de126c28b1
Joepolymath <joshuaajagbe96@gmail.com>
2022-07-17 22:28:20 +0100

    fixed styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import './Home.css'
-import Todo from './Todo.jsx'
+import React from "react";
+import "./Home.css";
+import Todo from "./Todo.jsx";
 
 const Home = () => {
   return (
     <div className="grid-rows-2 ">
-    
-    <div className="grid grid-cols-3 p-3 py-5 pb-4 text-white bg-neutral-700">
-      <a href="#" className='text-lg'>TodoApp</a>
-      <a href='#' className='text-stone-400 justify-self-center'>Home</a>
-      <a href='#' className='text-stone-400 justify-self-end'>Login</a>
-    </div>
-    <div className="grid-rows-2 p-8 ">
-      <div className="w-full border rounded box">
-        <div className='pl-5 text-xl border-b bg-neutral-100'>TODO LIST</div>
-        <div>
-          <form action="">
-            <input type="text" className='w-48 py-1 mt-8 border rounded' placeholder='enter text...'/>
-            <button className="w-20 h-8 ml-2 text-white bg-blue-500 rounded">Submit</button>
+      <div className="grid grid-cols-3 p-3 py-5 pb-4 text-white bg-neutral-700">
+        <a href="#" className="text-lg">
+          TodoApp
+        </a>
+        <a href="#" className="text-stone-400 justify-self-center">
+          Home
+        </a>
+        <a href="#" className="text-stone-400 justify-self-end">
+          Login
+        </a>
+      </div>
+      <div className="grid-rows-2 p-8 ">
+        <div className="w-full border rounded box">
+          <div className="pl-5 text-xl border-b bg-neutral-100">TODO LIST</div>
+          <div>
+            <form action="">
+              <input
+                type="text"
+                className="w-48 py-1 mt-8 border rounded"
+                placeholder="enter text..."
+                required
+              />
+              <button
+                type="submit"
+                className="w-20 h-8 ml-2 text-white bg-blue-500 rounded"
+              >
+                Submit
+              </button>
+            </form>
             <Todo />
-         </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './todo.css'
-import bin from '../images/bin.png'
+import React from "react";
+import "./todo.css";
+import bin from "../images/bin.png";
 
 const Todo = () => {
   return (
-    <div className='grid-rows-1'>
-      <div className='h-8 grid-cols-2 mt-8 border border-inherit'>
-        <button className='pt-8'>
-        <img src= {bin} alt="" className='w-5 opacity-40'/> 
+    <div className="grid-rows-1">
+      <div className="h-8 grid-cols-2 mt-8 border border-inherit flex items-center space-x-3 p-5">
+        <button className="">
+          <img src={bin} alt="" className="w-5 opacity-40" />
         </button>
-        <span className=''>First todo item</span>
+        <span className="">First todo item</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
